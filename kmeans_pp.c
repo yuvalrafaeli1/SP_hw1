47e3c846ba45d437ea5b6ab0f25de0fc2deb907e
yuvalrafaeli1 <116884388+yuvalrafaeli1@users.noreply.github.com>
2023-06-17 11:20:29 +0300

    updated functions to arrays

--- kmeans_pp.c
+++ kmeans_pp.c
@@ -2,7 +2,7 @@
 # include <stdlib.h>
 # include <math.h>
 
-/*this function need to recive = k (int), iter(int), eps(double), centroids ([][]double), vec_list([][]double)*/
+/*this function need to recive = dim(int), N(int), k (int), iter(int), eps(double), centroids ([][]double), vec_list([][]double)*/
 
  void error(){
         fprintf (stderr, "An Error Has Occurred!\n");
@@ -22,38 +22,32 @@ double comp_distance_arrays(double *cent_a, double *cent_b, int dim) {  /*calcul
     return sqrt(sum);
 }
 
-void assign_centroids(struct vector *head, int k, double **centroids){ /*need to change to arrays*/
-    struct vector *current_vector = head;
-    while (current_vector != NULL){
+void assign_centroids(int N, int dim, double **vectors, int k, double **centroids, int *cent_index){ /*changed to arrays*/
+    int j;
+    while (j<N){
         int i;
         for ( i=0 ; i<k; i++){
-            if (comp_distance(current_vector, centroids[i]) < (comp_distance(current_vector, centroids[current_vector->centroid])))
-                current_vector->centroid = i ;
+            if (comp_distance_arrays(vectors[j], centroids[i], dim) < (comp_distance_arrays(vectors[j], centroids[cent_index[j]], dim)))
+                centroids[cent_index[j]] = i ;
         }
-        current_vector = current_vector->next;
+        j++;
     }
 }
 
-double update_centroids(struct vector *head,int dim, int k, double **centroids) { /*need to change to arrays*/
+double update_centroids(int N,double **vectors,int dim, int k, double **centroids,int *cent_index) { /*changed to arrrays*/
     double max_change = -1, change;
     int i;
     double *new_i = malloc(sizeof(double)*dim);
     for (i=0; i < k; i++) { /*iterates every centroid*/ 
         
-        int cnt = 0,j,a;
-        struct vector *current_vector = head;
-        struct cord *current_cord;
-        
-        while (current_vector!= NULL) {
-            if (current_vector->centroid == i) {
+        int cnt = 0,j,a,s;
+        for (s=0; s < N; s++) {
+            if (cent_index[s] == i) {
                 cnt++;
-                current_cord = current_vector->cords;
                 for ( j = 0; j < dim; j++) {
-                    new_i[j] += current_cord->value;
-                    current_cord = current_cord->next;
+                    new_i[j] += vectors[s][j];
                 }
             }
-            current_vector = current_vector->next;
         }
         if(cnt!=0){
             for ( a = 0; a < dim; a++) {
@@ -98,35 +92,30 @@ void free_all(struct vector *head, int k, double **centroids){ /*need to change 
 
 
 int main (int argc, char *argv[]) {
-    int k, iter, N=0 , dim, d=1, i, j, curr_iter;
+    int k, iter, N , dim, d=1, i, j, curr_iter;
     double n, eps, max_change;
     char c;
-    struct vector *head_vec, *curr_vec;
-    struct cord *head_cord, *curr_cord;
-    double **centroids;
-
-    
-
-
-
+    double **centroids, **vectors;
+    int *cent_index;
 
     centroids = (double **)malloc(sizeof(double *) * k);
     for (i=0; i<k; i++){
         double *centroid = (double *)malloc(sizeof(double) * dim);
         centroids[i] = centroid;
     }
-
+                                    /*need to set cent_index to N zeros*/
                                     /*need to insert the centroids from python*/
+                                    /*need to update N, dim, k, iter, eps, vectors from python*/
+
                                     
     curr_iter = 1;
-    eps = 0.001;
-    assign_centroids(head_vec, k, centroids);
-    max_change = update_centroids(head_vec, dim, k, centroids);
+    assign_centroids(N, dim, vectors, k, centroids, cent_index);
+    max_change = update_centroids(N,vectors,dim ,k, centroids, cent_index);
 
     while ((max_change>=eps)&&(curr_iter<iter)){
         curr_iter++;
-        assign_centroids(head_vec, k, centroids);
-        max_change = update_centroids(head_vec, dim, k, centroids);
+        assign_centroids(N, dim, vectors, k, centroids, cent_index);
+        max_change = update_centroids(N,vectors,dim ,k, centroids, cent_index);
     }
                                     /* printing the result*/
     for(i=0; i<k; i++){
